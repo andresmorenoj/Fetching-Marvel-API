e056f8d9f041ba72d9a03efc9885743a98907bf7
= <=>
2020-11-12 10:11:29 -0500

    [ADD] Spinner component added

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,17 +1,32 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import * as comicsActions from '../redux/actions/comicsActions';
+import classNames from 'classname';
 import Comic from '../components/Comic';
+import Spinner from '../components/Spinner';
 import '../assets/styles/containers/Home.scss';
 
 const Home = (props) => {
+
   useEffect(() => {
     props.getAllComis();
   }, []);
 
+  const showComics = () => {
+    return props.comics.length > 0
+      ? props.comics.map((item) => (
+        <Comic
+          key={item.id}
+          comicImg={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+          comicTitle={item.title}
+        />
+      ))
+      : <Spinner />;
+  };
+
   return (
-    <section className='comics_container'>
-      {props.comics &&
+    <section className={props.comics < 0 ? 'spinner' : 'comics_container'}>
+      {/* {props.comics &&
         props.comics.map((item) => (
           <Comic
             key={item.id}
@@ -19,7 +34,7 @@ const Home = (props) => {
             comicTitle={item.title}
           />
         ))
-      }
+      } */ showComics()}
     </section>
   );
 };
--- src/redux/actions/comicsActions.js
+++ src/redux/actions/comicsActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const getAllComis = () => async dispatch => {
-  const response = await axios.get('https://gateway.marvel.com:443/v1/public/comics?apikey=bfef4b7aafd300840e8acb650288e0e3');
+  const response = await axios.get('https://gateway.marvel.com:443/v1/public/comics?apikey=bfef4b7aafd300840e8acb650288e0e3&limit=99');
   dispatch({
     type: 'GET_COMICS',
     payload: response.data.data.results
