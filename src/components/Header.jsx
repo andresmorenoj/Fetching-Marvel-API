d406b211761d780c9e2221c7d5174ec986b3ca10
= <=>
2020-11-12 01:34:37 -0500

    [ADD] Header component added and webpack configuration for images added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import HelloWorld from './HelloWorld';
+import { Link } from 'react-router-dom';
+import logoMarvel from '../assets/static/logoMarvel.png';
+import '../assets/styles/components/Header.scss';
 
-const Header = () => <h1>Soy el header</h1>;
+const Header = () => (
+  <header className='header'>
+    <Link to='https://www.marvel.com/'>
+      <img className='logo' src={logoMarvel} alt="" />
+    </Link>
+  </header>
+);
 
 export default Header;
